c26a7882f4cad586eacf5b96edc89e0ae8b9e0a9
Raguram-Gp <raguram10hb@gmail.com>
2022-02-23 21:10:52 +0530

    navbar completed for both mobile and desktop view

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -29,14 +29,15 @@ const Navbar = () => {
             transition={{ duration: 0.85, ease: "easeOut" }}
           >
             <hiX onClick={() => setToggle(false)} />
-            {["home", "about", "work", "skills", "contact"].map((item) => (
-              <li key={{ item }}>
-                <div />
-                <a href={`#${item}`} onClick={() => setToggle(false)}>
-                  {item}
-                </a>
-              </li>
-            ))}
+            <ul>
+              {["home", "about", "work", "skills", "contact"].map((item) => (
+                <li key={{ item }}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </motion.div>
         )}
       </div>
