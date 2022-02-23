a996f717d1838b8ff36cc69b29fb5a2cd09e0fe7
Raguram-Gp <raguram10hb@gmail.com>
2022-02-23 11:33:31 +0530

    started Navbar

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
-
 import { Navbar } from "./components";
+import "./App.scss";
 
 const App = () => {
   return (
--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
 import "./Navbar.scss";
+import { images } from "../../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <img src={images.logo} alt="logo" />
+        <ul>
+          {["home", "about", "work", "skills", "contact"].map((item) => (
+            <li key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
